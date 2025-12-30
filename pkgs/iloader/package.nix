d2c94523b7adb1784f385037c231047252cc6b66
ern775 <eren.demir2479090@gmail.com>
2025-12-30 07:24:36 +0300

    Update iloader

--- pkgs/iloader/package.nix
+++ pkgs/iloader/package.nix
@@ -12,11 +12,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "iloader";
-  version = "v1.1.5";
+  version = "1.1.6";
 
   src = fetchurl {
-    url = "https://github.com/nab138/iloader/releases/download/${finalAttrs.version}/iloader-linux-amd64.deb";
-    hash = "sha256-XSakTcf/uZ+nNsedpe90vrWgy50G0UGnl7JK09t1xOg=";
+    url = "https://github.com/nab138/iloader/releases/download/v${finalAttrs.version}/iloader-linux-amd64.deb";
+    hash = "sha256-Jd4Jb1rrYccVGUtqKLXe2uK7WZBxTkkYWjIPGOe1VMc=";
   };
 
   unpackCmd = "dpkg -x $curSrc source";
